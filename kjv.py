f0f055e1df861f7435876b81f09644faa3565385
Princess Pocon <pocon.evangeline@gmail.com>
2021-12-20 14:18:32 +0800

    remove hindi kailangan na files

--- kjv.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-# KING JAMES VERSION
-with open('./csv/kjv.csv','r') as csv_kjv_file:
-    csv_kjv_reader = csv.DictReader(csv_kjv_file)
--- tagab.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-# TAGALOG VERSION
-with open('./csv/tagab.csv','r') as csv_tagab_file:
-    csv_tagab_reader = csv.DictReader(csv_tagab_file)
